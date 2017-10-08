1fab5e894661f01a225862cac141ace007993eed
Christian Fusco <christian.fusco@mymail.champlain.edu>
2017-10-08 18:35:36 -0400

    Added headers and uml diagram.

--- Assignment3UML/src/Subject.java
+++ Assignment3UML/src/Subject.java
@@ -1,3 +1,22 @@
+/*Author: Robin Shafto and Christian Fusco
+Class: CSI-480-01/02
+Assignment: Assignment3 - Observer Pattern
+Date Assigned: 9/28/2017
+Due Date: 10/5/2017
+
+Description:
+An example of the observer/subject pattern using an auctioneer.  An auction is made and given an item, and then bidders try to win the item.  Each bidder has a unique strategy that either focuses on certain items, or stops bids past a certain point.
+
+Certification of Authenticity: 
+I certify that this is entirely my own work, except where I have given 
+fully-documented references to the work of others. I understand the definition 
+and consequences of plagiarism and acknowledge that the assessor of this 
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+*/
 interface Subject {
     void addObserver(Observer newObserver);
     void notifyObservers();
