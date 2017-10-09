9ed79d7dfdff1a70a2cc351c880130eb9b5e662b
Christian Fusco <christian.fusco@mymail.champlain.edu>
2017-10-09 18:20:51 -0400

    Updated with example for class.  Restructing 2.

--- Builder_Example/Design_Question/src/HelloWorld.java
+++ Builder_Example/Design_Question/src/HelloWorld.java
@@ -1,8 +1,27 @@
+/*Author: Christian Fusco and Robin Shafto
+Class: CSI-480-01/02
+Assignment: Builder Presentation
+Date Assigned: ???
+Due Date: 10/5/2017
+
+Description:
+Updated presentation with example code for a class assignment.  A hotdog is a sandwich. Builder example.
+
+Certification of Authenticity: 
+I certify that this is entirely my own work, except where I have given 
+fully-documented references to the work of others. I understand the definition 
+and consequences of plagiarism and acknowledge that the assessor of this 
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+*/
 public class HelloWorld {
     public static void main(String []args) {
         SandwichBuilder boring = new SandwichBuilder();
         Sandwich sandwich = boring.build();
-        System.out.println(sandwich.toString());
+        System.out.println("\n\n\n" + sandwich.toString() + "\n");
 
         SandwichBuilder moreFun = new SandwichBuilder();
         moreFun.setName("The Usual")
@@ -13,13 +32,13 @@ public class HelloWorld {
         sandwich = moreFun.build();
 
         SandwichBuilder burger = new HamburgerBuilder();
-        System.out.println(burger.build().toString());
+        System.out.println(burger.build().toString() + "\n");
 
         SandwichBuilder cheeseBurger = new HamburgerBuilder();
         cheeseBurger.addContents("cheese");
-        System.out.println(cheeseBurger.build().toString());
+        System.out.println(cheeseBurger.build().toString() + "\n");
 
         SandwichBuilder hotDog = new HotDogBuilder();
-        System.out.println(hotDog.build().toString());
+        System.out.println(hotDog.build().toString() + "\n");
     }
 }
--- Builder_Example/Design_Question/src/SandwichBuilder.java
+++ Builder_Example/Design_Question/src/SandwichBuilder.java
@@ -1,3 +1,22 @@
+/*Author: Christian Fusco and Robin Shafto
+Class: CSI-480-01/02
+Assignment: Builder Presentation
+Date Assigned: ???
+Due Date: 10/5/2017
+
+Description:
+Updated presentation with example code for a class assignment.  A hotdog is a sandwich. Builder example.
+
+Certification of Authenticity: 
+I certify that this is entirely my own work, except where I have given 
+fully-documented references to the work of others. I understand the definition 
+and consequences of plagiarism and acknowledge that the assessor of this 
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+*/
 import java.util.ArrayList;
 
 class SandwichBuilder {
